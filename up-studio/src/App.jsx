b0384a0856b673e7bcdddef6dbebd3cee16feba0
Weldon Carlos <weldonprogrammer2022@gmail.com>
2023-04-01 15:58:14 -0300

    Aqui removi as importações de topo e foot provisoriamente

--- up-studio/src/App.jsx
+++ up-studio/src/App.jsx
@@ -1,15 +1,14 @@
 
 import { Outlet } from 'react-router-dom';
 import './App.css';
-import Topo from './components/navbar/Topo';
-import Carrocel from './components/carrocel/Carrocel';
+
 
 
 function App() {
   
   return (
     <div className="App">
-      <Topo/>
+   
    
         <div className='container'>
           <Outlet/>
