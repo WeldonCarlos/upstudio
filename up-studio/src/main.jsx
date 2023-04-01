4201204a36d9da8b2131a3148d63b94a5e673221
Weldon Carlos <weldonprogrammer2022@gmail.com>
2023-04-01 11:05:09 -0300

    corrigindo erro de escrita

--- up-studio/src/main.jsx
+++ up-studio/src/main.jsx
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-import {createBrowserRoute,RouterProvider,Route} from 'react-router-dom'
+import {createBrowserRouter,RouterProvider,Route} from 'react-router-dom'
 
 // seção de rotas das paginas
 import Home from './routes/home/Home';
 import Sobre from './routes/sobre/Sobre';
-import Servicos from './routes/servicos/Servicos';
-import Portifolio from '.routes/portifolio/Portifolio';
-import Contato from '.routes/contato/Contato';
+import Servicos from './routes/servicos/Servicos'
+import Portifolio from './routes/portifolio/Portifolio'
+import Contato from './routes/contato/Contato'
 
 
 
-const router = createBrowserRoute([
+const router = createBrowserRouter([
   {
     element: <App/>,
     children:[
