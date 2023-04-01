b120cf35d5e630ea6142e3072c2cdb9305464cd6
Weldon Carlos <weldonprogrammer2022@gmail.com>
2023-04-01 01:24:47 -0300

    Importando e criando as rotas

--- up-studio/src/main.jsx
+++ up-studio/src/main.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+
+import {createBrowserRoute,RouterProvider,Route} from 'react-router-dom'
+
+const router = createBrowserRoute([
+  {
+    element: <App/>,
+    children:[
+      {
+        path:"/",
+      },
+      {
+        path:"/sobre",
+      },
+      {
+        path:"/portifolio",
+      },
+      {
+        path:"/contato",
+      },
+    ],
+  }
+
+])
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
